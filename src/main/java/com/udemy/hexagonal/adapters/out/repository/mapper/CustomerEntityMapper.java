7d16d3cc7463facb6e7eb8e47f225b924c331121
Gabriel Soares de Souza <gsoaresdesouza@live.com>
2025-10-26 17:31:42 -0300

    Criando Adapter de inserção do cliente

---
+++ src/main/java/com/udemy/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,11 @@
+package com.udemy.hexagonal.adapters.out.repository.mapper;
+
+import com.udemy.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.udemy.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+
+    CustomerEntity toCustomerEntity(Customer customer);
+}
