d1351cb95f278b011782b1779d0862dcd1d8aee3
Gabriel Soares de Souza <gsoaresdesouza@live.com>
2025-11-01 20:06:37 -0300

    Criando as configurações do Kafka

--- src/main/java/com/udemy/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
+++ src/main/java/com/udemy/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "FindAddressByZipCodeClient", url = "${arantes.client.address.url}")
+@FeignClient(name = "FindAddressByZipCodeClient", url = "${udemy.client.address.url}")
 public interface FindAddressByZipCodeClient {
 
     @GetMapping("/{zipCode}")
